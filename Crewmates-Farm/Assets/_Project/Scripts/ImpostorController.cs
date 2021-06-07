06be2dc9a57c2ca051c51978ede523f0d1400e22
Gisha <gishathedev@gmail.com>
2021-06-07 13:59:47 +0300

    add simple movement for impostor

--- Crewmates-Farm/Assets/_Project/Scripts/ImpostorController.cs
+++ Crewmates-Farm/Assets/_Project/Scripts/ImpostorController.cs
@@ -9,11 +9,29 @@ namespace Gisha.CrewmatesFarm.Core
         [Header("Movement")]
         [SerializeField] private float moveSpeed = 10f;
 
-        public Vector3 MoveInput => new Vector3(joystick.Horizontal,0f, joystick.Vertical);
+        public Vector3 MoveInput => new Vector3(joystick.Horizontal,0f, joystick.Vertical).normalized;
+
+        private Rigidbody _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
 
         private void Update()
         {
             Debug.Log(MoveInput);
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.velocity = MoveInput * moveSpeed;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(transform.position, MoveInput);
         }
     }
 }
